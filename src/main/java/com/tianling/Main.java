bd3579fd9881df2e0cde00122161e1f6aecad8fe
Ling Tian <859073143@qq.com>
2021-04-08 10:04:28 +0800

    Initial commit

---
+++ src/main/java/com/tianling/Main.java
@@ -0,0 +1,12 @@
+package com.tianling;
+
+/**
+ * @author Tianling
+ * @email 859073143@qq.com
+ * @since 2021/4/8 9:48
+ */
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
